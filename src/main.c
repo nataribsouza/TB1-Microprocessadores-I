9db8e30e689adb409c30cd07ba215b40451baaeb
Natã A. R. de Souza <nata.ards@gmail.com>
2024-11-24 16:05:23 -0300

    .

--- src/main.c
+++ src/main.c
@@ -5,16 +5,17 @@
 
 /* Main function */
 int main(void) {
-  
-  // Init setup
+  /* Init setup */
+
+  // Timer setup
   timer_init();
 
   // IO's setup
   init_pin(5, ENUM_PORT_PORTB, ENUM_PINMODE_OUTPUT);
 
-  // Main loop
+  /* Main loop */
   while(true) {
     set_pin(5, ENUM_PORT_PORTB, !read_pin(5, ENUM_PORT_PORTB));
-    delay(1000);
+    delay(10);
   }
 }
